13d56c5e8801607566c306781a1d2f37b6a82a9f
ajay <ajaybaloni01@gmail.com>
2025-06-29 21:18:36 +0530

    login page

---
+++ frontend/expense-tracker/src/pages/Dashboard/Expense.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Expense = () => {
+  return <div>Expense Expense</div>;
+};
+
+export default Expense;
---
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Honme</div>;
+};
+
+export default Home;
